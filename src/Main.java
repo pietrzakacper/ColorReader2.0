253ce0bd1fa09dc7d1f13ca489bb11b594b1f7fc
Kacper Pietrzak <kacpietrzak@gmail.com>
2018-11-01 15:41:30 +0100

    Add feature to overwrite colors and make code colors buffer size independent. Refactor

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,39 @@ public class Main {
 
         System.out.println("Hello!");
         System.out.println("Welcome to Color Reader 2.0!");
-        System.out.println("You will be able to check channels of maximum five colors.");
+        System.out.println("You will be able to save channels of maximum five colors.");
         System.out.println("Please insert color in form of the 32-bit integer or type 'exit' to exit the program.");
 
+        int[] colors = new int[5];
+        int colorsCount = 0;
 
-        String[] colors = new String[6];
         Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i<colors.length; i++){
+        while(true) {
+            System.out.print("Type color number " + (colorsCount+1) + ": ");
             String textColor = input.next();
 
-            if(textColor.equals("exit"))
-                i = colors.length;
-
-            else {
-                int color = Integer.parseInt(textColor);
-                colors[i] = "These are channels of color " + color + "\n" +
-                        "A: " + ((color >> 24) & 0b11111111) + "\n" +
-                        "R: " + ((color >> 16) & 0b11111111) + "\n" +
-                        "G: " + ((color >> 8) & 0b11111111) + "\n" +
-                        "B: " + (color & 0b11111111);
-
-                System.out.println(colors [i]);
-
-                if (i==5) {
-                    System.out.println("You have checked channels of the color five times. Rerun the program to check more.");
-                }
-                else {
-                    System.out.println("You can insert " + (5 - i) + " more colors, or type 'exit' to exit the program");
-                }
+            if(textColor.equals("exit")) {
+                break;
             }
+
+            colors[colorsCount] = Integer.parseInt(textColor);
+            colorsCount++;
+
+            for(int i=0; i<colorsCount; i++) {
+                System.out.println((i + 1) + ". Color: " + colors[i]);
+                System.out.println("\tA: " + ((colors[i] >> 24) & 0b11111111));
+                System.out.println("\tR: " + ((colors[i] >> 16) & 0b11111111));
+                System.out.println("\tG: " + ((colors[i] >> 8) & 0b11111111));
+                System.out.println("\tB: " + (colors[i] & 0b11111111));
+            }
+
+            colorsCount %= colors.length;
         }
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println(".");
-        System.out.println();
+
+        System.out.println(".\n.\n.\n.\n");
         System.out.println("Thanks for using Color Reader 2.0 See you soon :)");
+
+        input.close();
     }
 }
